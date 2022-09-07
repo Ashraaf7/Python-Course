092b7a57bd9627f5f5b67ba595df2d36029c5b73
Ahmed Ashraf <ahmedashraaf09@gmail.com>
2022-09-07 13:12:05 +0200

    First 3 Lessons

---
+++ Comments.py
@@ -0,0 +1,12 @@
+# This is a printed message 
+print("Ahmed Ashraf")
+'''
+print("Ahmed Ashraf")
+print("Ahmed Ashraf")
+print("Ahmed Ashraf")
+a
+a
+a
+a
+g
+'''
---
+++ Variables.py
@@ -0,0 +1,18 @@
+# camelCase
+nameOfPlayer= "Ahmed"  
+
+#PascalCase
+NameOfPlayer = "Ashraf"
+
+#snake_case
+name_of_player = "Ali"
+
+x , y ,z = 5,10 , 15 
+print(x)
+print(y)
+print(z)
+
+a=b=c = "Ahmed"
+print(a)
+print(b)
+print(c)
