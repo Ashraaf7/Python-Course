bb03b89eaf6d68113055fbc8d5a6d5ad1e617a41
Ahmed Ashraf <ahmedashraaf09@gmail.com>
2023-02-08 20:24:27 +0200

    Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,34 @@
+'''dict = {
+    1 : "JAN" ,
+    2 : "FEB" ,
+    3 : "MAR"
+}
+# [1,2,3,4,5]
+print(dict[3])
+'''
+'''
+boy = {
+    "Name" : "Ahmed" ,
+    "Age" : 20 
+}
+print(boy.get("grade"))  #boy[ ]
+
+
+dict1 = dict([(1,"one"),(2,"two"),(3,"three")])
+print(dict1)
+
+dict2 = dict(one=1,two=2,three=3)
+print(dict2)
+
+dict3 = dict({1:"one" , 2:"two"})
+print(dict3)
+'''
+
+programming = {
+    "Java" : "95%" ,
+    "Python" : "90%"
+}
+# java > 70%   python >80%
+programming["Java"] = "70%" 
+programming["Python"] = "80%"
+print(programming)
